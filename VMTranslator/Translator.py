ea0cba56a44d4e25b1603d1f693231d2e5cb7ab1
Votre Nom <rui-pedro.gomes-cardoso@etu-univ-orleans.fr>
2024-10-03 09:40:29 +0200

    Ajout des commentaire

--- VMTranslator/Translator.py
+++ VMTranslator/Translator.py
@@ -1,4 +1,3 @@
-"""No comment"""
 import os
 import glob
 import sys
@@ -7,16 +6,23 @@ import Generator
 
 
 class Translator:
-    """No comment"""
+    """Une classe pour traduire des fichiers VM en assembleur."""
 
     def __init__(self, files, asm):
+        """Initialise le traducteur avec les fichiers à traduire et un fichier de sortie pour l'assembleur.
+
+        Args:
+            files (str): Le chemin du fichier ou du dossier contenant les fichiers VM à traduire.
+            asm (str): Le nom du fichier de sortie où l'assembleur traduit sera écrit.
+        """
         self.asm = open(asm, "w")
         self.files = files
 
     def translate(self):
-        """No comment"""
+        """Traduit un ou plusieurs fichiers VM en assembleur et les écrit dans le fichier de sortie."""
+        # Écrire le code d'initialisation (bootstrap) dans le fichier assembleur.
         self.asm.write(self._bootstrap())
-        # os.listdir("/home/olivier")
+        # Vérifie si le chemin correspond à un fichier, sinon parcourt tous les fichiers .vm dans un répertoire.
         if os.path.isfile(self.files):
             self._translateonefile(self.files)
         else:
@@ -25,16 +31,27 @@ class Translator:
                     self._translateonefile(file)
 
     def _translateonefile(self, file):
-        """No comment"""
+        """Traduit un fichier VM unique et écrit le code généré dans le fichier assembleur.
+
+        Args:
+            file (str): Le chemin du fichier VM à traduire.
+        """
+        # Ajoute un commentaire dans le fichier assembleur pour indiquer le fichier en cours de traduction.
         self.asm.write(f"""\n//code de {file}\n""")
+        # Crée un générateur pour parcourir les commandes du fichier VM et les écrire dans l'assembleur.
         generator = Generator.Generator(file)
         for command in generator:
             self.asm.write(command)
 
     def _bootstrap(self):
-        """No comment"""
+        """Génère le code d'initialisation (bootstrap) en assembleur.
+
+        Retourne:
+            str: Le code d'initialisation en assembleur, qui initialise le pointeur de pile et appelle 'Sys.init'.
+        """
         init = Generator.Generator()._commandcall({'type': 'Call', 'function': 'Sys.init', 'parameter': '0'})
 
+        # Le code assembleur initialise le pointeur de pile à l'adresse 256 et appelle la fonction 'Sys.init'.
         return f"""// Bootstrap
     @256
     D=A
