fdfdabe0f80ac2731a433ade7c83c8f5e3e432a2
o22304722 <guerric.faure-bignolas@etu.univ-orleans.fr>
2024-10-10 10:02:01 +0200

    debut PushPop à compléter grace à BOB

--- VMTranslator/PushPop.py
+++ VMTranslator/PushPop.py
@@ -2,23 +2,61 @@ class PushPop:
     
     def __init__(self, command):
         pass
-    
-    def _commandpushconstant(self, command, commande=None):
+
+    def commandpushpop(self, command):
+
+
+
+    def _commandpushconstant(self, command):
         """No comment"""
         parameter = command['parameter']
         return f"""\t//{command['type']} {command['segment']} {parameter}
-    @{commande}     // Charger la valeur ou la variable {commande}
-    D=A             // Stocker la valeur dans D (ou A pour une constante)
-    @SP             // Accéder à la pile
-    A=M             // Accéder à l'adresse pointée par SP
-    M=D             // Stocker la valeur de D au sommet de la pile
-    @SP             // Accéder à nouveau au pointeur de pile
-    M=M+1           // Incrémenter SP pour préparer le prochain emplacement"""
+        @{parameter}     // Charger la valeur ou la variable {parameter}
+        D=A             // Stocker la valeur dans D (ou A pour une constante)
+        @SP             // Accéder à la pile
+        A=M             // Accéder à l'adresse pointée par SP
+        M=D             // Stocker la valeur de D au sommet de la pile
+        @SP             // Accéder à nouveau au pointeur de pile
+        M=M+1           // Incrémenter SP pour préparer le prochain emplacement"""
 
-    def _commandcall(self, command):
+    def _commandpushsegment(selfself,command):
         """No comment"""
-        return f"""\t//{command['type']} {command['function']} {command['parameter']}
-    Code assembleur de {command}\n"""
+        segment=command['segment']
+        parameter = command['parameter']
+        if segment == 'local':
+            base='LCL'
+        elif segment == 'argument':
+            base='ARG'
+        elif segment == 'this':
+            base='THIS'
+        elif segment == 'that':
+            base='THAT'
+
+
+        return f"""\t//{command['type']} {segment} {parameter}
+        @{base}
+        D=M             
+        @{parameter}
+        A=D+A
+        D=M             // Charger la première valeur dans le registre D
+        @SP
+        A=M
+        M=D
+        @SP
+        M=M+1
+        """
+
+    def _commandpushstatic(self,command,commande=None):
+        """No comment"""
+        parameter = command['parameter']
+
+
+        
+        
+        
+        
+
+
 
 
 
