e880292ddfe6d91466ec33b606d6587f988828c3
Djamat <djamat@LenovoX>
2024-10-14 17:39:10 +0200

    Modification arithmeticologiques de la loi 13 alinea 3 du code penal

--- VMTranslator/ArithmeticLog.py
+++ VMTranslator/ArithmeticLog.py
@@ -13,54 +13,192 @@ class ArithmeticLog:
                 return self.sub(commande)
             case 'equal':
                 return self.equal(commande)
+            case 'greater':
+                return self.greater(commande)
+            case 'lower':
+                return self.lower(commande)
+            case 'negative':
+                return self.negative(commande)
+            case 'and':
+                return self.AND(commande)
+
 
     def add(self, commande):
         return f"""// add : {commande}
-        @SP           // Accéder au sommet de la pile
-        AM=M-1        // Déréférencer l'adresse du sommet, décrémenter SP
-        D=M           // Charger la première valeur dans le registre D
-        @SP           // Accéder à nouveau au sommet de la pile
-        AM=M-1        // Décrémenter SP pour obtenir la deuxième valeur
-        M=D+M         // Ajouter la valeur en D à la valeur sur la pile, stocker le résultat à l'adresse du sommet
-        @SP           // Accéder à nouveau au sommet
-        M=M+1         // Incrémenter SP pour préparer le sommet suivant
-        """
-
-    def equal(self, commande):
-        return f"""// equal : {commande}
-        @SP           // Accéder au sommet de la pile
-        AM=M-1        // Décrémenter SP et accéder à la première valeur
-        D=M           // Charger la première valeur dans D
-        @SP           // Accéder à nouveau à la pile
-        AM=M-1        // Décrémenter SP pour obtenir la deuxième valeur
-        D=D-M         // Soustraire la deuxième valeur de la première (D contient le résultat)
-        @EQUAL_TRUE_{commande['type']}  // Aller à l'étiquette EQUAL_TRUE si le résultat est zéro (égalité)
-        D;JEQ         // Si D == 0, alors les valeurs sont égales
-        @SP           // Si non égal, on place 0 sur la pile
-        A=M           // Accéder au sommet de la pile
-        M=0           // Placer 0 (faux) au sommet
-        @EQUAL_END_{commande['type']}   // Sauter à la fin
-        0;JMP
-    (EQUAL_TRUE_{commande['type']})
-        @SP           // Si égal, on place 1 sur la pile
+        @SP
         A=M
-        M=-1          // Placer -1 (vrai) au sommet
-    (EQUAL_END_{commande['type']})
-        @SP           // Incrémenter SP
-        M=M+1
+        A=A-1
+        D=M     //On stock dans D la deuxième valeur
+        M=0
+        @SP
+        M=M-1
+        @SP
+        A=M
+        A=A-1
+        M=D+M       //On ajoute la deuxième valeur à la première
         """
 
     def sub(self, commande):
         return f"""// sub : {commande}
-        @SP           // Accéder au sommet de la pile
-        AM=M-1        // Déréférencer l'adresse du sommet, décrémenter SP
-        D=M           // Charger la première valeur dans le registre D
-        @SP           // Accéder à nouveau au sommet de la pile
-        AM=M-1        // Décrémenter SP pour obtenir la deuxième valeur
-        M=M-D         // Soustraire la première valeur de la deuxième, stocker le résultat à l'adresse du sommet
-        @SP           // Accéder à nouveau au sommet
-        M=M+1         // Incrémenter SP pour préparer le sommet suivant
+        @SP
+        A=M
+        A=A-1
+        D=M     //On stock dans D la deuxième valeur
+        M=0
+        @SP
+        M=M-1
+        @SP
+        A=M
+        A=A-1
+        M=M-D       //On soustrait la deuxième valeur à la première
         """
+
+    def equal(self, commande):
+        return f"""// equal : {commande}
+        @SP
+        //Aller a la première valeur
+        A=M
+        A=A-1
+        D=M     //On stock dans D la deuxième valeur
+        A=A-1
+        M=M-D       //On stock la différence entre les deux valeurs à l'emplacement de la première
+        @SP
+        M=M-1
+        A=M
+        M=0     //On enlève la deuxième valeur
+        //Mettre l'emplacement de la première valeur à -1 si elle est différente de 0
+        @SP     //Début condition JUMP (si v1=V2)
+        A=M
+        A=A-1
+        D=M
+        @END
+        D;JNE       //Fin condition JUMP (si v1!=V2)
+        @0
+        D=A-1       //D = -1
+        @SP
+        A=M
+        A=A-1
+        M=D     //Emplacement de la première valeur = -1
+        (END)
+        """
+
+    def greater(self, commande):
+        return f"""// greater: {commande}
+        //Vérifie si val1 > val2
+        //Val1=-1 if true else 0
+        @SP
+        A=M
+        A=A-1
+        D=M     //On stock dans D la deuxième valeur
+        M=0
+        @SP
+        M=M-1       //val2 dépilée
+        @SP
+        A=M
+        A=A-1
+        M=M-D       //On soustrait la deuxième valeur à la première
+        D=M
+        @IF_TRUE
+        D;JGT
+        @SP
+        A=M
+        A=A-1
+        M=0
+        @END
+        0;JMP
+        (IF_TRUE)
+        @0
+        D=A-1
+        @SP
+        A=M
+        A=A-1
+        M=D
+        (END)
+        """
+
+    def lower(self, commande):
+        return f"""// greater: {commande}
+                //Vérifie si val1 > val2
+                //Val1=-1 if true else 0
+                @SP
+                A=M
+                A=A-1
+                D=M     //On stock dans D la deuxième valeur
+                M=0
+                @SP
+                M=M-1       //val2 dépilée
+                @SP
+                A=M
+                A=A-1
+                M=M-D       //On soustrait la deuxième valeur à la première
+                D=M
+                @IF_TRUE
+                D;JLT
+                @SP
+                A=M
+                A=A-1
+                M=0
+                @END
+                0;JMP
+                (IF_TRUE)
+                @0
+                D=A-1
+                @SP
+                A=M
+                A=A-1
+                M=D
+                (END)
+                """
+
+    def negative(self, commande):
+        return f"""// negative: {commande}
+        @SP
+        A=M
+        A=A-1
+        D=M
+        M=M-D
+        M=M-D
+        """
+
+    def AND(self, commande):
+        return f"""// and: {commande}
+        @SP
+        A=M
+        A=A-1
+        D=M     //D=val2
+        M=0
+        @SP
+        M=M-1
+        @FALSE
+        D=D+1
+        D;JNE       //Si val2=false goto FALSE
+        @SP
+        A=M
+        A=A-1
+        D=M     //D=val1
+        @FALSE
+        D=D+1
+        D;JNE       //Si val=false goto FALSE
+        //Début condition true
+        @0
+        D=A-1
+        @SP
+        A=M
+        A=A-1
+        M=D
+        @END
+        0;JMP
+        //Fin condition true
+        //Début condition false
+        (FALSE)
+        D=0
+        @SP
+        A=M
+        A=A-1
+        M=D
+        (END)
+        """
+
 
 
 commande = {'type': 'add'}
